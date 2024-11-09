20106ffaa9b000ae3ffc74a1c2504622026f7bf8
Jeff <jeff@beargroup.com>
2024-11-08 19:16:26 -0800

    Implemented core logic to search products by command and print variants by price

---
+++ app.js
@@ -0,0 +1,24 @@
+const { program } = require('commander')
+const { searchProducts } = require('./src/queries')
+require('dotenv').config()
+
+program
+  .option('-name <value>')
+  .parse(process.argv)
+
+// Init
+const init = async () => {
+  const { Name: name = '' } = program.opts()
+  const products = await searchProducts(name)
+
+  // Loop through products, sort variants by price, and print text
+  products.forEach(product => {
+    product.variants.sort(
+      (a, b) => a.price - b.price
+    ).forEach(variant => {
+      console.log(`${product.title} - ${variant.title} - $${variant.price}`)
+    })
+  })
+}
+
+init()
